4ee271cf22896a0e6b4033d4d330e39132d6947e
GHOST <kenryalonzo@gmail.com>
2023-11-17 18:15:27 +0100

    fix: I refactored everything in my code, I modified it entirely, now it corresponds to what I had in mind so I hope it will suit you too.

--- main.py
+++ main.py
@@ -1,50 +1,65 @@
 import random
 import time
+import os
 
-# Fonction pour générer une séquence aléatoire de chiffres
-def generer_sequence(longueur):
-    sequence = []
-    for _ in range(longueur):
+# Fonction pour effacer les informations à l'écran
+def clear_screen():
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+
+def generer_sequence():
+    sequence = ""
+    for _ in range(4):
         chiffre = random.randint(0, 9)
-        sequence.append(chiffre)
+        sequence += str(chiffre)
     return sequence
 
-# Fonction pour demander à l'utilisateur de mémoriser la séquence
-def demander_sequence(sequence):
-    print("Mémorisez la séquence suivante :")
-    print("".join(map(str, sequence)))
-    time.sleep(3)  # Attendre 3 secondes pour permettre à l'utilisateur de mémoriser la séquence
-    # Effacer la console pour masquer la séquence
-    print("\033c")
+def afficher_sequence(sequence):
+    clear_screen()
+    print("Retenez la séquence :")
+    time.sleep(1)
+    print(sequence)
+    time.sleep(3)
+    clear_screen()
 
-# Fonction pour permettre à l'utilisateur de deviner la séquence
-def deviner_sequence(sequence):
-    print("Entrez la séquence que vous avez mémorisée (sans espaces) :")
-    reponse = input()
-    reponse = [int(chiffre) for chiffre in reponse]
-    if reponse == sequence:
-        return True
-    else:
-        return False
+def demander_reponse():
+    sequence_user = input("Votre réponse : ")
+    return sequence_user
 
-# Boucle principale du jeu
-def jouer_jeu():
-    sequence_base = generer_sequence(4)
-    sequence = sequence_base.copy()
-    longueur = 4
+def afficher_resultat(score):
+    clear_screen()
+    print("Bonne réponse, votre score est :", score)
+    time.sleep(1)
+
+def afficher_score_final(sequence_correcte, score):
+    clear_screen()
+    print("Mauvaise réponse, la séquence était :", sequence_correcte)
+    print("Votre score final :", score)
+
+score = 0
+rejouer = True
+
+while rejouer:
+    sequence_initiale = generer_sequence()
+    sequence = sequence_initiale
     score = 0
+
     while True:
-        demander_sequence(sequence)
-        if deviner_sequence(sequence):
-            chiffre = random.randint(0, 9)
-            sequence.append(chiffre)
-            longueur += 1
+        afficher_sequence(sequence)
+        sequence_user = demander_reponse()
+
+        if sequence_user == sequence:
             score += 1
+            sequence += str(random.randint(0, 9))
+            afficher_resultat(score)
         else:
-            print("Dommage. Vous vous êtes trompé.")
-            print("Votre score final est de", score)
-            print("La dernière séquence était :", "".join(map(str, sequence)))
+            afficher_score_final(sequence, score)
             break
 
-# Lancer le jeu
-jouer_jeu()
+    rejouer_input = input("Voulez-vous rejouer ? (oui/non) : ")
+    if rejouer_input.lower() != "oui":
+        rejouer = False
+    else:
+        continue
